e960f4c9f77a1120517d0dc01f6e4ba44f00e772
risk.limits@gmail.com <risk.limits@gmail.com@152a321b-45cb-b31c-fafc-6b351cb292cb>
2011-09-25 00:50:50 +0000

    Sample scripts for extracting publication information

---
+++ trunk/wok_client.py
@@ -0,0 +1,51 @@
+"""
+    This is an experimental script for interacting with the SOAP interface
+    of Thomson Reuters Web of Knowledge (formerly Web of Science) Lite interface which is
+    available to organizations which subscribe to it.
+
+    It requires that you have the Python SUDS library installed.
+
+"""
+
+import suds
+import logging
+import pprint
+logging.basicConfig(level=logging.INFO)
+
+class WebOfKnowledgeService(object):
+    def __init__(self):
+        self.wok_auth_uri = "http://search.isiknowledge.com/esti/wokmws/ws/WOKMWSAuthenticate?wsdl"
+        self.wok_uri = "http://search.isiknowledge.com/esti/wokmws/ws/WokSearchLite?wsdl"
+
+    def open(self):
+        self.auth_client = suds.client.Client(self.wok_auth_uri)
+        self.auth_result = self.auth_client.service.authenticate()
+
+        self.wok_client = suds.client.Client(self.wok_uri)
+        self.wok_client.set_options(headers = {"Cookie" : '[SID="%s"]' % self.auth_result})
+        self.auth_client.set_options(headers = {"Cookie" : '[SID="%s"]' % self.auth_result})
+    def close(self):
+        result = self.auth_client.service.closeSession()
+        logging.info(result)
+
+    def search(self, search_query, record_count = 100, first_record = 1):
+        queryParameters = self.wok_client.factory.create("queryParameters")
+        queryParameters.databaseID = "WOS"
+        queryParameters.queryLanguage = "en"
+        queryParameters.userQuery = search_query
+        queryParameters.editions= [{"collection" : "WOS", "edition" : "SCI"}]
+
+
+        retrieveParameters = self.wok_client.factory.create("retrieveParameters")
+        retrieveParameters.count = record_count
+        retrieveParameters.firstRecord = first_record
+
+        result = self.wok_client.service.search(queryParameters,retrieveParameters)
+        return result
+
+
+woks = WebOfKnowledgeService()
+woks.open()
+r = woks.search("TI=(The impact of conservation on the status of the world's vertebrates)")
+pprint.pprint(r)
+woks.close()
