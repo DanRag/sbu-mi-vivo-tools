2e44c376fa316c07b2424dba91f70db3b1536717
risk.limits@gmail.com <risk.limits@gmail.com@152a321b-45cb-b31c-fafc-6b351cb292cb>
2013-08-07 16:23:50 +0000

    Started to put infrastructure in place for loading directly into Virtuouso.

--- trunk/build_vivo_analytic_site.py
+++ trunk/build_vivo_analytic_site.py
@@ -2,8 +2,8 @@
     Builds an analytical RDF store that is based off of an RDF dump of a single or multiple VIVO
     installations.
 
-    Reads in a configuration file in json format. See sample 'configuration.json' for example of formatting the file
-    for a single VIVO installation.
+    Reads in a configuration file in json format. See sample 'configuration.json'
+    for example of formatting the file for a single VIVO installation.
 """
 
 __author__ = 'Janos G. Hajagos'
@@ -21,12 +21,13 @@ import pmid_to_cuis
 import umls_alignment
 import FreeTextTriples
 
+
 def main(configuration_json,step_to_start_at = 0):
 
-    f=open(configuration_json,"r")
+    f = open(configuration_json,"r")
     configuration = json.load(f)
     f.close()
-    i=0
+    i = 0
     for vivo_site in configuration["vivo-sites"]:
         base_file_name = vivo_site["name"]
         current_date_odbc = time.strftime("%Y-%m-%d",time.localtime())
@@ -36,7 +37,7 @@ def main(configuration_json,step_to_start_at = 0):
         vivo_site["tbox_file_name"] = tbox_file_name
 
         if step_to_start_at == 0:
-            print("Dumping the ontology from '%s' to '%s'" % (vivo_site["site"],os.path.abspath(tbox_file_name)))
+            print("Dumping the ontology from '%s' to '%s'" % (vivo_site["site"], os.path.abspath(tbox_file_name)))
             extract_rdf_data_from_vivo_site.main(vivo_site["site"], vivo_site["username"], vivo_site["password"],tbox="TBOX", file_name=tbox_file_name, abox=None)
             print("Dumping instance data from '%s' to '%s'" % (vivo_site["site"],os.path.abspath(abox_file_name)))
             extract_rdf_data_from_vivo_site.main(vivo_site["site"], vivo_site["username"], vivo_site["password"],abox="ABOX", file_name=abox_file_name, tbox=None)
@@ -57,17 +58,17 @@ def main(configuration_json,step_to_start_at = 0):
         vivo_site["free_text_indices"] = free_text_indices
         i += 1
 
-
         if step_to_start_at <= 3:
             print("Getting CUIS for articles")
             pmid_to_cuis.main(abox_file_name, abox_file_name + "." + "pmid2cuis.nt")
         else:
             pass
-
         vivo_site["pmid2cuis"] = abox_file_name + "." + "pmid2cuis.nt"
 
-
     pprint.pprint(configuration)
+
+def load_onto_virtuoso():
+    pass
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
--- trunk/extract_hierarchy_from_umls.py
+++ trunk/extract_hierarchy_from_umls.py
@@ -7,10 +7,15 @@ import os
 
 url_get_tail = lambda x: x[x.rfind("/")+1:]
 
-def main(ntriple_source_file,sab_class,child_of_predicate,file_name_prefix = ""):
+
+def main(ntriple_source_file, sab_class, child_of_predicate, file_name_prefix=""):
+
+    if sab_class.__class__ == list:
+        pass
+    else:
+        sab_class = [sab_class]
 
     fu = open(ntriple_source_file,"r")
-
     ts = SimpleTripleStore()
 
     print("Loading triples")
@@ -23,21 +28,21 @@ def main(ntriple_source_file,sab_class,child_of_predicate,file_name_prefix = "")
     graph_obj.register_class()
     graph_obj.register_node_predicate("<http://link.informatics.stonybrook.edu/umls/AUI/CODE>", "code")
 
-    graph_obj.register_node_predicate("<http://link.informatics.stonybrook.edu/umls/hasCUI>","CUI",url_get_tail)
-    graph_obj.register_node_predicate("<http://link.informatics.stonybrook.edu/umls/hasSAB>","SAB",url_get_tail)
-    graph_obj.register_node_predicate("<http://link.informatics.stonybrook.edu/umls/hasTermType>","TTY",url_get_tail)
+    graph_obj.register_node_predicate("<http://link.informatics.stonybrook.edu/umls/hasCUI>", "CUI", url_get_tail)
+    graph_obj.register_node_predicate("<http://link.informatics.stonybrook.edu/umls/hasSAB>", "SAB", url_get_tail)
+    graph_obj.register_node_predicate("<http://link.informatics.stonybrook.edu/umls/hasTermType>", "TTY", url_get_tail)
 
-    base_patterns = [("aui1","sab_predicate","sab"), ("aui1","childOf", "aui2"),("aui1","sab_predicate","sab")]
-    base_restrictions = [("sab","in",[sab_class]),("childOf", "in", [child_of_predicate])]
+    base_patterns = [("aui1","sab_predicate","sab"), ("aui1","childOf", "aui2"), ("aui2","sab_predicate","sab")]
+    base_restrictions = [("sab", "in", sab_class), ("childOf", "in", [child_of_predicate])]
 
     print("Writing results to a file")
 
-    graph_obj.add_pattern_for_links(base_patterns, base_restrictions, ["aui1","aui2"],"childOf")
+    graph_obj.add_pattern_for_links(base_patterns, base_restrictions, ["aui1", "aui2"], "childOf")
     directory,ntriple_source_base_file_name = os.path.split(ntriple_source_file)
     graphML_base_file_name = file_name_prefix + ntriple_source_base_file_name + ".network.hierarchy.graphml"
-    graphML_file_name = os.path.join(directory,graphML_base_file_name)
+    graphML_file_name = os.path.join(directory, graphML_base_file_name)
 
-    fo = open(graphML_file_name,"w")
+    fo = open(graphML_file_name, "w")
     graph_obj.translate_into_graphml_file(fo)
     fo.close()
 
@@ -48,6 +53,6 @@ if __name__ == '__main__':
 #    parent_term = sys.argv[3]
 #    preferred_term_filter =  sys.argv[4]
 
-    #main("C:/users/janos/data/umls/umls_full_isf_micro.nt","<http://link.informatics.stonybrook.edu/umls/SAB/ICD9CM>","<http://link.informatics.stonybrook.edu/umls/REL#CHD>",file_name_prefix="icd9cm_")
+    main("C:/users/janos/data/umls/umls_full_isf_micro.nt","<http://link.informatics.stonybrook.edu/umls/SAB/ICD9CM>","<http://link.informatics.stonybrook.edu/umls/REL#CHD>",file_name_prefix="icd9cm_")
     #main("C:/users/janos/data/umls/umls_full_isf_micro.nt","<http://link.informatics.stonybrook.edu/umls/SAB/NCI>","<http://link.informatics.stonybrook.edu/umls/RELA#isa>",file_name_prefix="nci_")
     main("/home/janos/rdf/umls_full_isf.nt","<http://link.informatics.stonybrook.edu/umls/SAB/MSH>","<http://link.informatics.stonybrook.edu/umls/REL#CHD>",file_name_prefix="msh_")
